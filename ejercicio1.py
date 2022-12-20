6f165b3dbd6ea843a6fd83153b05a63d20445e84
JulianC06 <william.castaneda03@uptc.edu.co>
2022-12-19 19:24:04 -0500

    ultima validacion

--- ejercicio1.py
+++ ejercicio1.py
@@ -4,28 +4,29 @@ newList=list()
 #función para eliminar los números según sea el limite que se ingrese por parámetro
 def deleteNumbersInLimit(listNumbers,limitS):
 
-    listNumbers=orderReverse(listNumbers)
+    if(len(listNumbers)<=100):
+        listNumbers=orderReverse(listNumbers)
     
-    #solo se agrega el número una vez. Ej. para limit 6, el número 596 entraría dos veces, entonces con esta variable
-    #se limita a que entre una sola vez.
-    #también esta variable revisa que si no hay un digito mayor al limit, entonces no es necesario dividir el número
-    #por ejemplo, para limit 6, el número 301, no debería agregarse como [3,0,1] sino como [301]
-    isDivisble=False
-
-    #recorrer la lista general
-    for number in listNumbers:
-        #recorrer número por número (1980 -> 1,9,8,0)
-        for numberStr in str(number):
-            if((int(numberStr)>=limitS) and (not isDivisble)):
-                isDivisble=True
-                divisibleNumber(number,limitS)
-                #newList.append(int(numberStr))
+        #solo se agrega el número una vez. Ej. para limit 6, el número 596 entraría dos veces, entonces con esta variable
+        #se limita a que entre una sola vez.
+        #también esta variable revisa que si no hay un digito mayor al limit, entonces no es necesario dividir el número
+        #por ejemplo, para limit 6, el número 301, no debería agregarse como [3,0,1] sino como [301]
+        isDivisble=False
+    
+        #recorrer la lista general
+        for number in listNumbers:
+            #recorrer número por número (1980 -> 1,9,8,0)
+            for numberStr in str(number):
+                if((int(numberStr)>=limitS) and (not isDivisble)):
+                    isDivisble=True
+                    divisibleNumber(number,limitS)
+                    #newList.append(int(numberStr))
         
-        if(not isDivisble):
-            newList.append(number)
-        else:
-            isDivisble=False
-    print(newList)
+            if(not isDivisble):
+                newList.append(number)
+            else:
+                isDivisble=False
+        print(newList)
 
 
 #función que se encarga de agregar digito por digito a la lista de resultado en dado caso que exista un digito mayor al limit
@@ -43,7 +44,3 @@ def orderReverse(listNumbers):
     return reverseLIst
 
 deleteNumbersInLimit([60, 6, 5, 4, 3, 2, 7, 7, 29, 1],2)
-#array1=[1,2,3,4]
-
-#array1.reverse()
-#print(orderReverse(array1))
